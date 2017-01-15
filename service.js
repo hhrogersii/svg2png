0a9b59b61281a8805c85e97183a6471d4621d781
Henry Rogers <hrogers@sugarcrm.com>
2017-01-14 21:39:09 -0500

    Use PhantomJS server module (no PHP dependencies)

---
+++ service.js
@@ -0,0 +1,95 @@
+var port = 9494;
+var server = require('webserver').create();
+// var fs = require('fs');
+// var url = 'file://' + fs.absolute('./chart.html');
+var url = 'chart.html';
+
+var chartRender = function(config, datum) {
+        // Define chart model & class name
+        var model = nv.models[config.type + 'Chart']();
+        var classname = 'nv-chart-' + config.type;
+
+        // Apply configuration settings
+        // Get method names from config keys
+        var methods = Object.getOwnPropertyNames(config);
+        // This will be replaced with a model.config(config) in future release
+        for (var i = 0, l = methods.length; i < l; i += 1) {
+            var method = methods[i];
+            if (model[method]) {
+                model[method](config[method]);
+            }
+        }
+
+        // Add chart type class to container
+        d3.select('#chart_container').classed(classname, true);
+        // Bind data to svg and call model
+        d3.select('#chart_container svg')
+            .datum(datum)
+            .call(model);
+    };
+
+var service = server.listen(port, function(request, response) {
+    var post = JSON.parse(request.post);
+    var chartConfig = post.CHART_CONFIG;
+    var reportDatum = post.REPORT_DATUM;
+    var page = require('webpage').create();
+    var handleError = function(code, msg) {
+            response.statusCode = code;
+            response.write(msg);
+            response.close();
+            page.close();
+            phantom.exit();
+        };
+
+    // Set up response
+    response.setHeader('Content-Type', 'text/plain');
+    // Since service is running on a domain different than request
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Define boundaries of capture region
+    page.clipRect = {
+            top: 0,
+            left: 0,
+            width: 600,
+            height: 600
+        };
+    page.viewportSize = {
+            width: 600,
+            height: 600
+        };
+    page.zoomFactor = 1;
+
+    page.open(url, function (status) {
+
+        if (status !== 'success') {
+            handleError(500, 'Unable to load chart page.');
+            return;
+        }
+
+        try {
+            // Render chart with config and data
+            page.evaluate(chartRender, chartConfig, reportDatum);
+        } catch(e) {
+            handleError(500, e.message);
+            return;
+        }
+
+        // Everything's A-OK
+        response.statusCode = 200;
+
+        // Send encoded image
+        response.write(page.renderBase64('PNG'));
+        // Close to complete request
+        response.close();
+
+        // Close to release memory
+        page.close();
+    });
+});
+
+if (service) {
+  console.log('Web server running on port ' + port);
+} else {
+  console.log('Error: Could not create web server listening on port ' + port);
+  phantom.exit();
+}
---
+++ test/chart.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$headers = apache_request_headers();
+
+if (isset($headers['CHART_CONFIG'])) {
+  // If page is called by service screen capture with header variables
+
+  $chartConfig = json_decode($headers['CHART_CONFIG']);
+  $reportDatum = $headers['REPORT_DATUM'];
+
+} else {
+  // else define variables for testing
+
+  $chartConfig = json_decode(json_encode(array(
+    'type' => 'pie',
+    'direction' => 'ltr',
+    'showTitle' => true,
+    'showLegend' => true,
+    'showLabels' => true,
+    'tooltips' => false,
+    'donut' => true,
+    'donutRatio' => 0.5,
+    'hole' => 10,
+    'maxRadius' => 250,
+    'minRadius' => 100,
+    'rotateDegrees' => 0,
+    'arcDegrees' => 360,
+    'colorData' => 'default',
+  )));
+
+  $reportDatum = file_get_contents('../resources/pie_data.json');
+
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=600" />
+  <title>Chart</title>
+  <link rel="stylesheet" type="text/css" href="../resources/nvd3_print.css" />
+  <style>
+    body {
+      background-color: #eef5ff;
+    }
+    #chart_export {
+      width: 600px;
+      height: 600px;
+    }
+  </style>
+</head>
+<body>
+
+<div id="chart_export" class="nv-chart nv-chart-<?php echo $chartConfig->type; ?>">
+  <svg></svg>
+</div>
+
+<script src="../resources/d3.min.js"></script>
+<script src="../resources/nv.d3.min.js"></script>
+
+<script type="text/javascript">
+  // Define define config and data variables
+  var chartConfig = <?php echo json_encode($chartConfig); ?>;
+  var reportDatum = <?php echo $reportDatum; ?>;
+
+  // Define chart model
+  var chartModel = nv.models[chartConfig.type + 'Chart']();
+
+  // Apply configuration settings
+  // Get method names from config keys
+  var methods = Object.getOwnPropertyNames(chartConfig);
+  // This will be replaced with a chartModel.config(chartConfig) in future release
+  for (var i = 0, l = methods.length; i < l; i += 1) {
+    var method = methods[i];
+    if (chartModel[method]) {
+      chartModel[method](chartConfig[method]);
+    }
+  }
+
+  // Bind data to svg and call model
+  d3.select('#chart_export svg')
+    .datum(reportDatum)
+    .call(chartModel);
+</script>
+</body>
+</html>
